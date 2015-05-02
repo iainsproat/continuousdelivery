a20dc9070981fac8399fe33c11c91680475ef056
Iain Sproat <iainsproat@gmail.com>
2015-05-02 22:29:53 +0400

    Test framework
    NUnit test framework added to project.
    Simply tests that a file (hard linked) contains and does not contain
    certain text.

---
+++ Test/Test/CheckReport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Test
+{
+	[TestFixture]
+	public class CheckReport
+	{		
+		[Test]
+		public void DoesNotContainBanana()
+		{
+			const string fileName = Utilities.relativeBaseDirectory + @"Reports/Structural/SD Report.txt";
+			string contents = Utilities.ReadFile(fileName);
+			StringAssert.Contains("Structural report", contents, "Could not find file");
+			StringAssert.DoesNotContain("banana", contents, "The document is not allowed to contain the word 'banana'");
+		}
+	}
+}
---
+++ Test/Test/Utilities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace Test
+{
+	public static class Utilities
+	{
+		public const string relativeBaseDirectory = @"../../../../";
+		
+		public static string ReadFile(string fileName){
+			try
+			{
+				using (var sr = new StreamReader(fileName))
+				{
+					String contents = sr.ReadToEnd();
+					return contents;
+				}
+			}
+			catch (Exception e)
+			{
+				Assert.Fail("Could not read file\n {0}", e.StackTrace);
+				return string.Empty;
+			}
+			
+		}
+	}
+}
