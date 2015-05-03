23ff3c144b0eb51ac3ad08120fca61f2fc3543bf
Iain Sproat <iainsproat@gmail.com>
2015-05-03 11:27:25 +0400

    Renamed Reports to 6.0 Reports
    Resolves #1

--- Test/Test/CheckReport.cs
+++ Test/Test/CheckReport.cs
@@ -9,7 +9,7 @@ namespace Test
 		[Test]
 		public void DoesNotContainBanana()
 		{
-			const string fileName = Utilities.relativeBaseDirectory + @"Reports/Structural/SD Report.txt";
+			const string fileName = Utilities.relativeBaseDirectory + @"6.0 Reports/Structural/SD Report.txt";
 			string contents = Utilities.ReadFile(fileName);
 			StringAssert.Contains("Structural report", contents, "Could not find file");
 			StringAssert.DoesNotContain("banana", contents, "The document is not allowed to contain the word 'banana'");
